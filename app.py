8e2409cdadba9e6cf63434dca0c091cfd57897d0
Pavan Pera <63477400+pavan9130@users.noreply.github.com>
2022-07-26 13:17:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request,send_from_directory,current_app,send_file
+#importing required libraries
+
+from flask import Flask, render_template,session, request,send_from_directory,current_app,send_file
 import json
 import pickle
 import json
@@ -11,11 +13,15 @@ lemmatizer = WordNetLemmatizer()
 import numpy as np
 from tensorflow.keras.models import load_model
 
-intents = json.loads(open('intents.json').read())
+
+#reading intents.json file and loading pickle files and model file
+intents_file = open('intents.json',encoding='utf-8').read()
+intents = json.loads(intents_file)
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
 model = load_model('chatbot_model.h5')
 
+#this function takes english sentence and gives  base word list 
 def clean_up_sentence(sentence):
     # tokenize the pattern - splitting words into array
     sentence_words = nltk.word_tokenize(sentence)
@@ -24,7 +30,7 @@ def clean_up_sentence(sentence):
     return sentence_words
 
 
-# return bag of words array: 0 or 1 for words that exist in sentence
+# return bag of words array: 0 or 1 for words that exist in sentence taking reference of our 'words file'
 def bag_of_words(sentence, words, show_details=True):
     # tokenizing patterns
     sentence_words = clean_up_sentence(sentence)
@@ -41,7 +47,7 @@ def bag_of_words(sentence, words, show_details=True):
 
 
 
-
+#this will predict all probabilities of each tag and selects max probability
 def predict_class(sentence):
     # filter below  threshold predictions
     p = bag_of_words(sentence, words,show_details=False)
@@ -53,8 +59,10 @@ def predict_class(sentence):
     return_list = []
     for r in results:
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+        break
     return return_list
 
+#this will get response based on tag
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
     #print(tag)
@@ -65,7 +73,8 @@ def getResponse(ints, intents_json):
             break
     return result
 
-
+# this takes user input and sends to above functions and sends back to HTML
+# this also handle other functions 
 def GetAnswer(user_response):
     answer=[]
     if user_response !='bye' and user_response !='yes' and user_response !='no':
@@ -73,13 +82,23 @@ def GetAnswer(user_response):
                 for (index, d) in enumerate(ints):
                    answer.append(d['intent'])                 
                 if (len(ints)==0):
-                 answer.append('Sorry I am still learning')
+                 answer.append('status')
+                 answer.append('Sorry I am still learning. Please elaborate or ask other Question!')
                 else:
                  answer.append(getResponse(ints, intents))
+                 session['question']=user_response
+                 session['response']=getResponse(ints, intents)
     elif(user_response=='yes' or user_response=='YES' ):
         answer.append('status')
         answer.append('Good to know that')
     elif(user_response=='no' or user_response=='NO' ):
+        q = open("Questions.txt", "a")
+        q.write("\n")
+        q.write('Q'+':'+session['question'])
+        q.write("\n")
+        q.write('A'+':'+session['response'])
+        q.write("\n")
+        q.close()
         answer.append('status')
         answer.append('Sorry I am still learning . I will update myself soon')
     else:
@@ -91,7 +110,9 @@ def GetAnswer(user_response):
 app = Flask(__name__,template_folder='Templates')
 app.static_folder = 'static'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.secret_key = 'chatbot123'
 
+#this will start log file and also renders HTML
 @app.route("/")
 def main():
      f = open("myfile.txt", "a")
@@ -102,23 +123,48 @@ def main():
      f.close()
      return render_template("main.html")
 
+# this will get response and also created log file
 @app.route("/get")
 def get_bot_response():
      userText = request.args.get('msg')
+     userText=userText.lower()
+     li=GetAnswer(userText)
      f = open("myfile.txt", "a")
      #f.write('%s'%_datetime.datetime.today())
      #f.write("\n")
      f.write('You'+':'+userText)
      f.write("\n")
-     f.write('Robo'+':'+str(GetAnswer(userText)[1]))
+     f.write('Robo'+':'+str(li[1]))
      f.write("\n")
      f.close()
-     return json.dumps(GetAnswer(userText))
-@app.route("/downloadFile")
-def downloadFile ():
+     return json.dumps(li)
+
+#This will download only unsatisfied questions
+
+@app.route("/downloadQAFile")
+def downloadQAFile ():
+    #For windows you need to use drive name [ex: F:/Example.pdf]
+    path = "Questions.txt"
+    return send_file(path, as_attachment=True, cache_timeout=0)
+
+#this will download entire conversation
+
+@app.route("/downloadLogFile")
+def downloadLogFile ():
     #For windows you need to use drive name [ex: F:/Example.pdf]
     path = "myfile.txt"
     return send_file(path, as_attachment=True, cache_timeout=0)
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 if __name__ == "__main__":
     app.run() 
